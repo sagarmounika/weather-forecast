9f54259b18217a6d1304a71d019b15146f3b7543
sagarmounika <sagarmouni5@gmail.com>
2024-02-17 18:22:55 +0530

    api key

--- src/Reducers/weatherSlice.js
+++ src/Reducers/weatherSlice.js
@@ -3,7 +3,7 @@ import {createAsyncThunk, createSlice} from "@reduxjs/toolkit"
 import axios from "axios"
 import {getFormattedWeatherData} from "../utilities/weatherFormatting"
 
-const API_KEY = "39ebde1d03084fba89494919241602"
+const API_KEY = "0ddcfdbe5ad640668c1122717241702"
 const BASE_URL = "https://api.weatherapi.com/v1"
 
 export const fetchWeatherData = createAsyncThunk(
