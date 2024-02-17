c3d7af4cb3952e0347ed8d6f7ed037e6369fe5d1
sagarmounika <sagarmouni5@gmail.com>
2024-02-17 17:48:49 +0530

    loading,error

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,36 +4,49 @@ import TimeAndLocation from "./TimeAndLocation"
 import TemperatureAndDetails from "./TemperatureAndDetails"
 import Forecast from "./Forecast"
 import WeekForecast from "./WeekForecast"
-import OtherDetails from "./OtherDetails.jsx"
+import {MdErrorOutline} from "react-icons/md"
 const Main = () => {
-  const {weatherData} = useSelector(state => state.weatherReducer)
+  const {weatherData, loading, error} = useSelector(
+    state => state.weatherReducer
+  )
   const {unit} = useSelector(state => state.searchReducer)
   return (
-    <>
+    <div className="flex flex-1 flex-col lg:flex-row text-xs md:text-base">
       {" "}
-      {weatherData && (
-        <div className="flex flex-1 flex-col lg:flex-row text-xs md:text-base">
-          <div className="w-full lg:w-1/3 border-r border-light-gray px-3">
-            <TimeAndLocation weather={weatherData} />
-            <TemperatureAndDetails weather={weatherData} unit={unit} />
-          </div>
-
-          <div className="flex-1 p-2 px-3">
-            <Forecast
-              title="hourly forecast"
-              unit={unit}
-              forecast={weatherData.hourlyForecast}
-            />
-            <WeekForecast
-              title="daily forecast"
-              unit={unit}
-              forecast={weatherData.dailyForecast}
-            />
-            {/* <OtherDetails weather={weatherData} unit={unit} /> */}
-          </div>
+      {loading ? (
+        <div className="flex items-center justify-center w-full   mt-10">
+          <img src="./loading.gif" alt="Logo" className="w-24 lg:w-auto" />
         </div>
+      ) : error ? (
+        <div className="flex items-center flex-col justify-center w-full   mt-10 p-5">
+          <MdErrorOutline size={30} />
+          <div className="text-dark-gray">No Data Available</div>
+        </div>
+      ) : (
+        weatherData && (
+          <>
+            <div className="w-full lg:w-1/3 border-r border-light-gray px-3">
+              <TimeAndLocation weather={weatherData} />
+              <TemperatureAndDetails weather={weatherData} unit={unit} />
+            </div>
+
+            <div className="flex-1 p-2 px-3">
+              <Forecast
+                title="hourly forecast"
+                unit={unit}
+                forecast={weatherData.hourlyForecast}
+              />
+              <WeekForecast
+                title="daily forecast"
+                unit={unit}
+                forecast={weatherData.dailyForecast}
+              />
+              {/* <OtherDetails weather={weatherData} unit={unit} /> */}
+            </div>
+          </>
+        )
       )}
-    </>
+    </div>
   )
 }
 
